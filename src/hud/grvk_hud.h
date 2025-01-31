f6f9af692571988d12d3ee7397ad889426713562
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-01-31 19:21:52 +0300

    Update grvk_hud.h

--- src/hud/grvk_hud.h
+++ src/hud/grvk_hud.h
@@ -5,10 +5,10 @@
 #include "grvk_hud_item.h"
 #include "grvk_hud_renderer.h"
 
-namespace dxvk::hud {
+namespace grvk::hud {
 
   /**
-   * \brief DXVK HUD
+   * \brief GRVK HUD
    * 
    * Can be used by the presentation backend to
    * display performance and driver information.
@@ -17,7 +17,7 @@ namespace dxvk::hud {
     
   public:
     
-    Hud(const Rc<DxvkDevice>& device);
+    Hud(const Rc<grvkDevice>& device);
     
     ~Hud();
 
@@ -37,8 +37,8 @@ namespace dxvk::hud {
      * \param [in] dstView Swap chain image view
      */
     void render(
-      const DxvkContextObjects& ctx,
-      const Rc<DxvkImageView>&  dstView);
+      const grvkContextObjects& ctx,
+      const Rc<grvkImageView>&  dstView);
 
     /**
      * \brief Checks whether the HUD is empty
@@ -64,16 +64,16 @@ namespace dxvk::hud {
      * \brief Creates the HUD
      * 
      * Creates and initializes the HUD if the
-     * \c DXVK_HUD environment variable is set.
-     * \param [in] device The DXVK device
+     * \c GRVK_HUD environment variable is set.
+     * \param [in] device The GRVK device
      * \returns HUD object, if it was created.
      */
     static Rc<Hud> createHud(
-      const Rc<DxvkDevice>& device);
+      const Rc<GrvkDevice>& device);
     
   private:
     
-    Rc<DxvkDevice>        m_device;
+    Rc<GrvkDevice>        m_device;
     
     HudRenderer           m_renderer;
     HudItemSet            m_hudItems;
